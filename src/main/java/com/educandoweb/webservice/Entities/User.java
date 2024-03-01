90f2d7ae73d039cd3c21a6c714df972229b2b53d
ViniCarmo <vinicius.carmo1106@gmail.com>
2024-03-01 16:34:28 -0300

    User Entity and Resource

---
+++ src/main/java/com/educandoweb/webservice/Entities/User.java
@@ -0,0 +1,78 @@
+package com.educandoweb.webservice.Entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+
+    private static final long serialVerionUID = 1L;
+    private Long Id;
+    private String nome;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User(){
+    }
+
+    public User(Long id, String nome, String email, String phone, String password) {
+        Id = id;
+        this.nome = nome;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(Id, user.Id) && Objects.equals(nome, user.nome) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+}
